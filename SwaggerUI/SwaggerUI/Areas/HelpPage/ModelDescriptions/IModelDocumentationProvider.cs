a9248fcedd9a88bf0a423de1f9e5097da869a7e9
Mehmet ÇAKMAZ <mehmetcakmaz@gmail.com>
2017-07-18 23:36:10 +0300

    second commit

---
+++ SwaggerUI/SwaggerUI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace SwaggerUI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ SwaggerUI/SwaggerUI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SwaggerUI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
